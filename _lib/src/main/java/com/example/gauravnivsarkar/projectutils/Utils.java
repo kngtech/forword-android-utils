19ad94df2e2acee4feba42421f612e62c8dad64b
Gaurav N <gaurav@heady.io>
2016-09-08 13:35:51 +0530

    fix confirm password validation error

--- _lib/src/main/java/com/example/gauravnivsarkar/projectutils/Utils.java
+++ _lib/src/main/java/com/example/gauravnivsarkar/projectutils/Utils.java
@@ -17,6 +17,7 @@ public class Utils {
     }
 
     public static final boolean isValidPassword(CharSequence target,int validLength,boolean isConfirm,CharSequence password){
+        if(TextUtils.isEmpty(password)) return false;
         boolean val= !TextUtils.isEmpty(target) && target.toString().trim().length()>=validLength;
         if(isConfirm) val&=password.equals(target);
         return val;
